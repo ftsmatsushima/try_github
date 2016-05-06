af4b2eb22120b6c81f4158eb5a057f58bcaf38ea
SPL-026 <SPL-026@SPL-026-PC>
2016-05-06 16:50:06 +0900

    標準出力へ文字を表示

--- src/try_github/Main.java
+++ src/try_github/Main.java
@@ -1,7 +1,8 @@
 package try_github;
 
 public class Main {
-	public static void Main(String[] args){
+	public static void main(String[] args){
+		System.out.println("Hello Git with Eclipse");
 		
 	}
 
